5cc6847c02d423dadf96050a482758d3b126ae25
Jay Parlar <parlar@gmail.com>
2015-04-02 14:20:18 -0400

    fix(configs): fix

--- src/configs/configs.js
+++ src/configs/configs.js
@@ -21,6 +21,7 @@ angular.module('encore.ui.configs', [])
 .constant('foobar999999', '/v1.14.0')
 .constant('foobar1.22.0', '/v1.14.0')
 .constant('foobar1.25.0', '/v1.14.0')
+.constant('foobar1.28.0', '/v1.14.0')
 .constant('feedbackApi', '/api/feedback')
 .provider('routesCdnPath', function () {
 
